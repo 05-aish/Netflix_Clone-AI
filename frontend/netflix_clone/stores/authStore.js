f13cacbb6d3100f038a128b740a4ca4ccc61a0fa
Aishwarya Mandal <mandal2021aish@gmail.com>
2026-05-24 18:24:13 +0530

    deployment changes

--- frontend/netflix_clone/stores/authStore.js
+++ frontend/netflix_clone/stores/authStore.js
@@ -2,120 +2,59 @@ import { create } from 'zustand';
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL; // add this one line
 
 export const useAuthStore = create((set) => ({
-    //initial state
     user: null,
     loading: false,
     error: null,
     message: null,
     isFetching: true,
 
-    //setters
-    signUp: async (username, email, password) => {set({
-        loading: true,
-        message: null
-    })
-    try {
-        const response = await axios.post('http://localhost:3001/api/signup', {
-            username, email, password
-        }) 
-
-        //user is userDoc
-        set({
-            user: response.data.user, 
-            loading: false
-        })
-    } catch (error) {
-        
-        set({
-            loading: false,
-            error: error.response.data.message || 'Error Signing Up.'
-        })
-
-        throw error;
-    }
-
+    signUp: async (username, email, password) => {
+        set({ loading: true, message: null })
+        try {
+            const response = await axios.post('/api/signup', { username, email, password }) 
+            set({ user: response.data.user, loading: false })
+        } catch (error) {
+            set({ loading: false, error: error.response.data.message || 'Error Signing Up.' })
+            throw error;
+        }
     },
 
-    login: async(username, password) => {set({
-        loading: true,
-        message: null,
-        error: null
-    })
-    try {
-        const response = await axios.post("http://localhost:3001/api/login", {
-            username,
-            password,
-        })
-        
-        const { user, message } = response.data;
-        set({
-            user,
-            message,
-            loading: false
-        });
-
-        return { user, message };
-
-    } catch (error) {
-        set({
-            loading: false,
-            error: error.response.data.message || 'Error Logging in.'
-        })
-
-        throw error;
-    }
+    login: async(username, password) => {
+        set({ loading: true, message: null, error: null })
+        try {
+            const response = await axios.post("/api/login", { username, password })
+            const { user, message } = response.data;
+            set({ user, message, loading: false });
+            return { user, message };
+        } catch (error) {
+            set({ loading: false, error: error.response.data.message || 'Error Logging in.' })
+            throw error;
+        }
     },
 
     fetchUser: async () => {
-        set({
-            isFetching: true,
-            error: null
-        })
-
+        set({ isFetching: true, error: null })
         try {
-            const response = await axios.get("http://localhost:3001/api/fetch");
-
-            set({
-                user: response.data.user,
-                isFetching: false 
-            })
+            const response = await axios.get("/api/fetch");
+            set({ user: response.data.user, isFetching: false })
         } catch (error) {
-            set({
-                isFetching: false,
-                error: error.response?.data?.message || 'Error fetching user.'
-            })
+            set({ isFetching: false, error: error.response?.data?.message || 'Error fetching user.' })
             throw error;
         }
     },
 
     logout: async () => {
-        set({
-            loading: true,
-            error: null,
-            message: null
-        })
-
+        set({ loading: true, error: null, message: null })
         try {
-          const response = await axios.post("http://localhost:3001/api/logout");
-          const {message} = response.data;
-
-          set({
-            message,
-            loading: false,
-            user: null,
-            error: null
-          })
-
-          return { message };
-
+            const response = await axios.post("/api/logout");
+            const { message } = response.data;
+            set({ message, loading: false, user: null, error: null })
+            return { message };
         } catch (error) {
-             set({
-            loading: false,
-            error: error.response?.data?.message || 'Error Logging out.'
-             })
-
+            set({ loading: false, error: error.response?.data?.message || 'Error Logging out.' })
             throw error;
         }
     }
