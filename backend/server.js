6198dae1d1fd4520a3baa271faf2a6c315aedde6
Aishwarya Mandal <mandal2021aish@gmail.com>
2026-05-24 18:16:07 +0530

    cookie strictness fixed

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,7 @@ app.post("/api/signup", async (req, res) => {
             res.cookie("token", token, {
                 httpOnly: true, 
                 secure: process.env.NODE_ENV === "production",
-                sameSite: "strict",
+                sameSite: "none",
 
             })
         }
